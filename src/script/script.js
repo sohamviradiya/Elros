fdff7beb44818f69330180a00fa0da247091df06
sohamviradiya <sohamviradiya2003@gmail.com>
2022-08-18 23:30:38 +0530

    add script

--- src/script/script.js
+++ src/script/script.js
@@ -3,6 +3,7 @@ class Task {
           this.title = title;
           this.description = description;
           this.dueDate = new Date(...dueDate.split('-').map((x) => (Number(x))));
+          this.dueDate.setMonth(this.dueDate.getMonth() - 1);
           this.setDate = new Date();
           this.priority = Number(priority);
           this.id = id;
@@ -26,7 +27,7 @@ class Project {
      constructor(title, id) {
           this.title = title;
           this.tasks = [];
-          this.currentTask = new Task('sample task', 'with description','2022-08-07',0);
+          this.currentTask = new Task('sample task', 'with description','2022-05-01',0);
           this.id = id;
           this.taskCount = 0;
      }
